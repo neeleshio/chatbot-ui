b99f97d0a1e7633904eb73994c5007fa56255f65
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-12-21 16:08:55 +0530

    bot-img-fix

--- src/views/Chatbox/Chatbox.js
+++ src/views/Chatbox/Chatbox.js
@@ -42,11 +42,9 @@ function Chatbox({ handleClose, open }) {
                         <p>I am Iris, a Virtual Assistant</p>
                         <p>How may I help you today?</p>
                     </div>
-                    {botImg && (
-                        <div className="intro-bot-img">
-                            <img src="./bot.svg" alt="bot" />
-                        </div>
-                    )}
+                    <div className="intro-bot-img">
+                        <img src="./bot.svg" alt="bot" />
+                    </div>
                 </div>
                 <div className="faq-wrapper">
                     <div className="bot-img">
